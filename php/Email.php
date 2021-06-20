8b60908bcd790bd1f0079a8883e02471ea2985e7
Dario Sbarbati <dario_386@hotmail.com>
2021-06-19 23:01:52 -0300

    1

--- php/Email.php
+++ php/Email.php
@@ -2,6 +2,17 @@
 $nombre = $_POST['nombre'];
 $email = $_POST['email'];
 $telefono = $_POST['telefono'];
+$mensaje = $_POST['mensaje'];
+
+$texto_concatenado = 'Nombre: '.$nombre.'Email: '.$email.' Telefono: '.$telefono.' Mensaje: '.$mensaje;
 
 echo $nombre
+
+if (mail('consultas@aldatec.com.ar', 'Consulta Web' , $texto_concatenado))
+{ echo "Mensaje enviado exitosamente";}
+else
+{
+  echo "Error al tratar de enviar el mensaje al servidor";
+}
+
 ?>
